f3a03c550920f1eb33f363a5a9ca3ba95b497cde
Thireach <Thireach988@gmail.com>
2020-06-08 10:12:02 +0700

    Adapter View Assignment

--- app/src/main/java/com/example/cookbookapp/RecipesActivity.java
+++ app/src/main/java/com/example/cookbookapp/RecipesActivity.java
@@ -29,19 +29,19 @@ public class RecipesActivity extends AppCompatActivity {
     }
     private Recipes[] loadRecipes() {
         Recipes recipe1 = new Recipes();
-        recipe1.getUser();
-        recipe1.getR_name();
-        recipe1.getR_description();
+        recipe1.setUser("Sarah");
+        recipe1.setR_name("Boiled Eggs");
+        recipe1.setR_description("First, fill a pot with water and boil the water till it bubbles. Then, slowly add your eggs in and cover the lid. Wait 5 minutes and your boiled eggs will be done.");
 
         Recipes recipe2 = new Recipes();
-        recipe2.getUser();
-        recipe2.getR_name();
-        recipe2.getR_description();
+        recipe2.setUser("Jake");
+        recipe2.setR_name("Pancakes");
+        recipe2.setR_description("You will need a pancake mix. Crack some eggs into a mixing bowl and whisk it until it becomes smooth. Then, pour the pancake mix in and stir until it is combined. Cook it under medium heat and finally, serve it. ");
 
         Recipes recipe3 = new Recipes();
-        recipe3.getUser();
-        recipe3.getR_name();
-        recipe3.getR_description();
+        recipe3.setUser("Peter");
+        recipe3.setR_name("Fried eggs");
+        recipe3.setR_description("Heat up some oil in a skillet. Crack and cook your eggs. Serve it with some bread or sauce. ");
 
         return new Recipes[] {recipe1,recipe2,recipe3};
     }
